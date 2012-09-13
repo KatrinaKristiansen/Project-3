931e245212d39a32d2424c33fc38080d7ad3113b
katrina kristiansen <kkristiansen@fullsail.edu>
2012-09-12 22:09:03 -0500

    project 3
    hope this works

---
+++ kristiansen_katrina_assignment3.js
@@ -0,0 +1,62 @@
+var jackKing = 2;
+function myFunction =();
+         alert(jackKing;
+        console.log(jackKing "Hi spooks and ghouls");{
+            function jackKingDance(){;
+                 jackKingDance() || jackKingSings();
+                 };
+        };
+        function sallyCries();
+        var cries - sitInRoom()
+        if (!cries){;
+            runsAway
+            
+        var jackScared =("Come back Sally");
+        jackRuns ("running to save sally");
+        
+        function jackRuns(jackScared)};
+             alert(jackScared); "running to save sally";
+             
+             };
+        function abc();
+        a = 22;
+        b = 0;
+        
+        };
+        var a = 0;
+        var b = "";
+        abc();
+        console.log("a = " = a +"and b ="+b);
+        
+        for (i=0; i<19; i++);
+        };
+        x=c + "Jack and Sally reunite" + i;
+        
+        var happy
+        if (a==b);
+        if (a==c);
+            happy= "Jack and Sally are happy!";
+            
+            } else {
+                happy = " Jack and Sally marry!";
+                
+            } else {
+                if (a==c);
+                happy = "Zero and Jack find Sally";
+                
+            } else {
+                if (b== c);
+                happy = "Jack never finds Sally";
+            } else {
+                happy = "Sad ending";
+
+                
+    var sallyLost = 15
+    while (sallyLost > 0)
+    console.log (sallyLost + " Sally running in Oogy never ending forrest. ");
+    sallyLost--;
+            };
+            
+    console.log("Sally find her way back to Jack!");       
+
+}
